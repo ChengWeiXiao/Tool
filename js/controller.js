61231e839c02a6f7a752604e4be8d642f64145d7
ChengWeiXiao <92635078+ChengWeiXiao@users.noreply.github.com>
2021-10-18 11:37:40 +0800

    Add files via upload

--- js/controller.js
+++ js/controller.js
@@ -44,15 +44,10 @@
         initial();
         var identityNumber = document.getElementById("identityNumber").value;
         if (identityNumber == null || identityNumber == undefined || identityNumber == '') {
-            swal({
-                title: "提醒",
-                text: "請填入身分證末三碼",
-                icon: "warning",
-                button: "關閉",
-            });
+            document.getElementById('invalid').style.display='block';
             return;
         }
-        localStorage.setItem('identityNumber', identityNumber.toString())
+
         var week1Count = 0;
         var week2Count = 0;
         var week3Count = 0;
@@ -68,124 +63,201 @@
         week3Result.innerHTML = '';
         week4Result.innerHTML = '';
 
-        if(winNo1.length>0){
+        if (winNo1.length > 0) {
             identityNumber.split(',').filter(onlyUnique).forEach(id => {
+                week1Count = 0;
                 winNo1.forEach(element => {
                     if (element.number.find(element => element == id.slice(-(element.length)))) {
                         let span = document.createElement('span');
-                        span.style.fontWeight='bold';
+                        span.style.fontWeight = 'bold';
 
-                        if(document.getElementById('week1Number'+id)==null){
+                        if (document.getElementById('week1Number' + id) == null) {
                             let numberSpan = document.createElement('span');
-                            numberSpan.style.fontWeight='bold';
-                            numberSpan.id='week1Number'+id;
+                            numberSpan.style.fontWeight = 'bold';
+                            numberSpan.id = 'week1Number' + id;
                             numberSpan.appendChild(document.createTextNode('輸入號碼:' + id));
                             week1Result.appendChild(numberSpan);
                         }
-                        
+
                         span.appendChild(document.createTextNode('中獎券別:' + element.name + element.lotteryDate));
                         week1Result.appendChild(document.createElement('br'));
                         week1Result.appendChild(span);
                         week1Count++;
                     }
                 });
+                if (week1Count == 0) {
+                    if (document.getElementById('week1Number' + id) == null) {
+                        let numberSpan = document.createElement('span');
+                        numberSpan.style.fontWeight = 'bold';
+                        numberSpan.id = 'week1Number' + id;
+                        numberSpan.appendChild(document.createTextNode('輸入號碼:' + id));
+                        week1Result.appendChild(numberSpan);
+                    }
+                    let winning = document.createElement('h4');
+                    winning.textContent = '下週還有機會!';
+                    winning.style.color = '#f0643a';
+                    winning.style.fontWeight = 'bold';
+                    week1Result.appendChild(winning);
+                } else {
+                    let noWinning = document.createElement('h4');
+                    noWinning.textContent = '恭喜你中獎了!';
+                    noWinning.style.color = '#04a776';
+                    noWinning.style.fontWeight = 'bold';
+                    week1Result.appendChild(noWinning);
+                }
                 week1Result.appendChild(document.createElement('hr'));
             });
         }
 
-        if(winNo2.length>0){
+        if (winNo2.length > 0) {
             identityNumber.split(',').filter(onlyUnique).forEach(id => {
+                week2Count = 0;
                 winNo2.forEach(element => {
                     if (element.number.find(element => element == id.slice(-(element.length)))) {
                         let span = document.createElement('span');
-                        span.style.fontWeight='bold';
+                        span.style.fontWeight = 'bold';
 
-                        if(document.getElementById('week2Number'+id)==null){
+                        if (document.getElementById('week2Number' + id) == null) {
                             let numberSpan = document.createElement('span');
-                            numberSpan.style.fontWeight='bold';
-                            numberSpan.id='week2Number'+id;
+                            numberSpan.style.fontWeight = 'bold';
+                            numberSpan.id = 'week2Number' + id;
                             numberSpan.appendChild(document.createTextNode('輸入號碼:' + id));
                             week2Result.appendChild(numberSpan);
                         }
-                        
+
                         span.appendChild(document.createTextNode('中獎券別:' + element.name + element.lotteryDate));
                         week2Result.appendChild(document.createElement('br'));
                         week2Result.appendChild(span);
                         week2Count++;
                     }
                 });
+                if (week2Count == 0) {
+                    if (document.getElementById('week2Number' + id) == null) {
+                        let numberSpan = document.createElement('span');
+                        numberSpan.style.fontWeight = 'bold';
+                        numberSpan.id = 'week2Number' + id;
+                        numberSpan.appendChild(document.createTextNode('輸入號碼:' + id));
+                        week2Result.appendChild(numberSpan);
+                    }
+                    let winning = document.createElement('h4');
+                    winning.textContent = '下週還有機會!';
+                    winning.style.color = '#f0643a';
+                    winning.style.fontWeight = 'bold';
+                    week2Result.appendChild(winning);
+                } else {
+                    let noWinning = document.createElement('h4');
+                    noWinning.textContent = '恭喜你中獎了!';
+                    noWinning.style.color = '#04a776';
+                    noWinning.style.fontWeight = 'bold';
+                    week2Result.appendChild(noWinning);
+                }
                 week2Result.appendChild(document.createElement('hr'));
             });
         }
 
-        if(winNo3.length>0){
+        if (winNo3.length > 0) {
             identityNumber.split(',').filter(onlyUnique).forEach(id => {
+                week3Count = 0;
                 winNo3.forEach(element => {
                     if (element.number.find(element => element == id.slice(-(element.length)))) {
                         let span = document.createElement('span');
-                        span.style.fontWeight='bold';
+                        span.style.fontWeight = 'bold';
 
-                        if(document.getElementById('week3Number'+id)==null){
+                        if (document.getElementById('week3Number' + id) == null) {
                             let numberSpan = document.createElement('span');
-                            numberSpan.style.fontWeight='bold';
-                            numberSpan.id='week3Number'+id;
+                            numberSpan.style.fontWeight = 'bold';
+                            numberSpan.id = 'week3Number' + id;
                             numberSpan.appendChild(document.createTextNode('輸入號碼:' + id));
                             week3Result.appendChild(numberSpan);
                         }
-                        
+
                         span.appendChild(document.createTextNode('中獎券別:' + element.name + element.lotteryDate));
                         week3Result.appendChild(document.createElement('br'));
                         week3Result.appendChild(span);
                         week3Count++;
                     }
                 });
+                if (week3Count == 0) {
+                    if (document.getElementById('week3Number' + id) == null) {
+                        let numberSpan = document.createElement('span');
+                        numberSpan.style.fontWeight = 'bold';
+                        numberSpan.id = 'week3Number' + id;
+                        numberSpan.appendChild(document.createTextNode('輸入號碼:' + id));
+                        week3Result.appendChild(numberSpan);
+                    }
+                    let winning = document.createElement('h4');
+                    winning.textContent = '下週還有機會!';
+                    winning.style.color = '#f0643a';
+                    winning.style.fontWeight = 'bold';
+                    week3Result.appendChild(winning);
+                } else {
+                    let noWinning = document.createElement('h4');
+                    noWinning.textContent = '恭喜你中獎了!';
+                    noWinning.style.color = '#04a776';
+                    noWinning.style.fontWeight = 'bold';
+                    week3Result.appendChild(noWinning);
+                }
                 week3Result.appendChild(document.createElement('hr'));
             });
         }
-        
-        if(winNo4.length>0){
+
+        if (winNo4.length > 0) {
             identityNumber.split(',').filter(onlyUnique).forEach(id => {
+                week4Count = 0;
                 winNo4.forEach(element => {
                     if (element.number.find(element => element == id.slice(-(element.length)))) {
                         let span = document.createElement('span');
-                        span.style.fontWeight='bold';
+                        span.style.fontWeight = 'bold';
 
-                        if(document.getElementById('week4Number'+id)==null){
+                        if (document.getElementById('week4Number' + id) == null) {
                             let numberSpan = document.createElement('span');
-                            numberSpan.style.fontWeight='bold';
-                            numberSpan.id='week4Number'+id;
+                            numberSpan.style.fontWeight = 'bold';
+                            numberSpan.id = 'week4Number' + id;
                             numberSpan.appendChild(document.createTextNode('輸入號碼:' + id));
                             week4Result.appendChild(numberSpan);
                         }
-                        
+
                         span.appendChild(document.createTextNode('中獎券別:' + element.name + element.lotteryDate));
                         week4Result.appendChild(document.createElement('br'));
                         week4Result.appendChild(span);
                         week4Count++;
                     }
                 });
+                if (week4Count == 0) {
+                    if (document.getElementById('week4Number' + id) == null) {
+                        let numberSpan = document.createElement('span');
+                        numberSpan.style.fontWeight = 'bold';
+                        numberSpan.id = 'week4Number' + id;
+                        numberSpan.appendChild(document.createTextNode('輸入號碼:' + id));
+                        week4Result.appendChild(numberSpan);
+                    }
+                    let winning = document.createElement('h4');
+                    winning.textContent = '下週還有機會!';
+                    winning.style.color = '#f0643a';
+                    winning.style.fontWeight = 'bold';
+                    week4Result.appendChild(winning);
+                } else {
+                    let noWinning = document.createElement('h4');
+                    noWinning.textContent = '恭喜你中獎了!';
+                    noWinning.style.color = '#04a776';
+                    noWinning.style.fontWeight = 'bold';
+                    week4Result.appendChild(noWinning);
+                }
                 week4Result.appendChild(document.createElement('hr'));
             });
         }
-        
-        if(week1Count>0){
+
+        if (week1Count > 0) {
             document.getElementById("week1").style.display = 'block';
         }
-        if(week2Count>0){
+        if (week2Count > 0) {
             document.getElementById("week2").style.display = 'block';
         }
-        if(week3Count>0){
+        if (week3Count > 0) {
             document.getElementById("week3").style.display = 'block';
         }
-        if(week4Count>0){
+        if (week4Count > 0) {
             document.getElementById("week4").style.display = 'block';
-        }
-        if (week1Count+week2Count+week3Count+week4Count > 0) {
-            document.getElementById("winning").style.display = 'block';
-
-        } else {
-            document.getElementById("noWinning").style.display = 'block';
-
         }
     }
 
@@ -195,29 +267,27 @@
 
     function removeValue() {
         document.getElementById("identityNumber").value = '';
-        document.getElementById("week1").style.display = 'none';
-        document.getElementById("week2").style.display = 'none';
-        document.getElementById("week3").style.display = 'none';
-        document.getElementById("week4").style.display = 'none';
         initial();
     }
 
     function initial() {
-        document.getElementById("winning").style.display = 'none';
-        document.getElementById("noWinning").style.display = 'none';
-
         let week1Result = document.getElementById('week1Result');
         let week2Result = document.getElementById('week2Result');
         let week3Result = document.getElementById('week3Result');
         let week4Result = document.getElementById('week4Result');
+
+        document.getElementById('invalid').style.display='none';
+        document.getElementById("week1").style.display = 'none';
+        document.getElementById("week2").style.display = 'none';
+        document.getElementById("week3").style.display = 'none';
+        document.getElementById("week4").style.display = 'none';
 
         week1Result.innerHTML = '';
         week2Result.innerHTML = '';
         week3Result.innerHTML = '';
         week4Result.innerHTML = '';
     }
-    let identityNumber=null; 
-    function loadUid() {
-        identityNumber = document.getElementById('identityNumber')
-        identityNumber.value = localStorage.getItem('identityNumber')
-      }
+
+    function clearInvalid(){
+        document.getElementById('invalid').style.display='none';
+    }
